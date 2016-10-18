8f4924993c2938fe71791f87f868851b22a3f317
Benoit Chabod <chabodb@tcd.ie>
2016-10-18 17:32:22 +0100

    Sprint 1 - Configure project for libGDX and box2D

---
+++ android/src/com/chabodb/carrot/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chabodb.carrot;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chabodb.carrot.CarrotGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CarrotGame(), config);
+	}
+}
